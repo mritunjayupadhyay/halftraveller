57730802f450270cc98a91e8d11eb2f3d53a6275
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2023-11-24 21:09:42 +0700

    feat: add a component header

---
+++ components/Header/header.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+
+function Header() {
+    return (
+        <header className="w-full flex justify-center items-center flex-col">
+            <h1>header</h1>
+        </header>
+    )
+}
+
+export default Header
